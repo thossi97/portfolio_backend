aac7f01b99b9832b619ddb31a1356757602d6357
thossi97 <tho85@hi.is>
2020-11-21 02:14:28 +0000

    picture test

--- home/models.py
+++ home/models.py
@@ -5,5 +5,5 @@ class Project(models.Model):
     name = models.CharField(max_length=30)
     image = models.ImageField(blank=True, upload_to="project_pics", default="default.jpg")
     #git = models.URLField()
-    url = models.URLField(blank=True, text="https://")
+    url = models.URLField(blank=True)
     description = models.TextField()
