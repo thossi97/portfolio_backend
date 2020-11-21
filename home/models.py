e56a30513930f0f3c0ef1e762a7fb9f1bd555c5d
thossi97 <tho85@hi.is>
2020-11-21 16:09:11 +0000

    git and demo field add

--- home/models.py
+++ home/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=30)
     image = models.ImageField(blank=True, default="default.jpg")
-    #git = models.URLField()
-    url = models.URLField(blank=True)
+    git = models.URLField()
+    demo = models.URLField(blank=True)
     description = models.TextField()
