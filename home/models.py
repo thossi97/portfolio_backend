c75f43d360c227c455812a0cb5bebec90ad167ee
thossi97 <tho85@hi.is>
2020-11-21 16:24:44 +0000

    upload to

--- home/models.py
+++ home/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(max_length=30)
-    image = models.ImageField(blank=True, default="default.jpg")
+    image = models.ImageField(blank=True, upload_to="project_pics", default="default.jpg")
     git = models.URLField(blank=True)
     url = models.URLField(blank=True)
     description = models.TextField()
